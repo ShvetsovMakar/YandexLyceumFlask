24f82d5a19e893a8cd173063f0d7a5c9a0aa0cc0
ShvetsovMakar <shved.mash@outlook.com>
2025-05-12 07:12:31 +0300

    added custom error messages

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def fetch_usernames():
     usernames = []
     cur.execute(f"SELECT username FROM users")
     for i in cur.fetchall():
-        usernames.append(i[0].lower())
+        usernames.append(i[0])
 
     return usernames
 
@@ -61,7 +61,7 @@ def signup():
         return redirect("/feed")
 
     if request.method == 'POST':
-        username = request.form['username']
+        username = request.form['username'].lower()
         password = request.form['password']
         confirmation = request.form['confirmation']
 
@@ -69,12 +69,15 @@ def signup():
             return redirect("/sign_up")  # password and confirmation do not match
 
         usernames = fetch_usernames()
-        if username.lower() in usernames:
+        if username in usernames:
             return redirect("/sign_up")  # username already exists
 
         # Adding user to the database
         cur.execute(f'SELECT MAX(id) FROM users')
-        user_id = cur.fetchone()[0] + 1
+        try:
+            user_id = cur.fetchone()[0] + 1
+        except (Exception,):
+            user_id = 1
 
         cur.execute(f"INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
                     (user_id, username, password, '', '', '', '', 'day'))
@@ -92,11 +95,11 @@ def login():
         return redirect("/feed")
 
     if request.method == 'POST':
-        username = request.form['username']
+        username = request.form['username'].lower()
         password = request.form['password']
 
         usernames = fetch_usernames()
-        if username.lower() not in usernames:
+        if username not in usernames:
             return redirect("/login")  # no such user
 
         cur.execute(f"SELECT password FROM users WHERE username = \"{username}\"")
@@ -182,18 +185,18 @@ def feed():
         cur.execute(f"SELECT mode FROM users WHERE id = {current_user.id}")
         mode = cur.fetchone()[0]
 
-    res = cur.execute("""Select * from posts""").fetchall()
+    res = cur.execute("SELECT * FROM posts").fetchall()
     posts = []
     for i in range(len(res)):
-        posts.append(Post(
-            id = res[i][0],
-            title = res[i][1],
-            content = res[i][2],
-            category = res[i][3],
-            creation_date = string_to_date(res[i][4]),
-            author = cur.execute(f"Select username from users Where id = ({res[i][5]})").fetchall()[0][0]
-
-        ))
+        posts.append(
+            Post(
+                id=res[i][0],
+                title=res[i][1],
+                content=res[i][2],
+                category=res[i][3],
+                creation_date=string_to_date(res[i][4]),
+                author=cur.execute(f"SELECT username FROM users WHERE id = ({res[i][5]})").fetchall()[0][0])
+        )
     return render_template(
         'feed.html',
         mode=mode,
@@ -209,7 +212,7 @@ def user_info(username):
 
     usernames = fetch_usernames()
     if username not in usernames:
-        return render_template("invalid_user.html", mode=mode)
+        return redirect("/invalid_user")
 
     cur.execute(f"SELECT name, surname, birth_date, about FROM users WHERE username = \"{username}\"")
     info = cur.fetchone()
@@ -220,6 +223,29 @@ def user_info(username):
 
     return render_template("user_info.html", mode=mode, username=username, name=name, surname=surname,
                            birth_date=birth_date, about=about)
+
+
+@app.route("/invalid_user", methods=['GET', 'POST'])
+def invalid_user():
+    mode = "day"
+    if current_user.is_authenticated:
+        cur.execute(f"SELECT mode FROM users WHERE id = {current_user.id}")
+        mode = cur.fetchone()[0]
+
+    return render_template("invalid_user.html", mode=mode)
+
+@app.errorhandler(401)
+def unauthorized_callback(e):
+    return render_template("login_required.html", mode="day")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    mode = "day"
+    if current_user.is_authenticated:
+        cur.execute(f"SELECT mode FROM users WHERE id = {current_user.id}")
+        mode = cur.fetchone()[0]
+
+    return render_template("page_not_found.html", mode=mode)
 
 
 if __name__ == "__main__":
